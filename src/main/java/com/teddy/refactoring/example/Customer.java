49ed7e2eab67a73513e1a75361dbc0b6c7ac4af4
TeddyH <rusdlsla@gmail.com>
2014-05-11 19:27:38 +0900

    conflict

--- src/main/java/com/teddy/refactoring/example/Customer.java
+++ src/main/java/com/teddy/refactoring/example/Customer.java
@@ -31,25 +31,16 @@ public class Customer {
 		String result = getName() + "고객님의 대여 기록\n";
 		
 		while (rentals.hasMoreElements()) {
-			double thisAmount = 0;
 			Rental each = rentals.nextElement();
-			
-			// 비디오 종류별 대여료 계산
-			thisAmount = this.amountFor(each);
 
-			// 적립 포인트를 1 포인트 증가
-			frequentRenterPoints++;
-			// 최신물을 이틀 이상 대여하면 보너스 포인트 지급 
-			if ((each.getMovie().getPriceCode() == Movie.NEW_RELEASE) && 
-					each.getDaysRented() > 1) {
-				frequentRenterPoints++;
-			}
+			// 경우에 따른 적립 포인트 지급 함수를 호출
+			frequentRenterPoints += each.getFrequentRenterPoints();
 			
 			// 이번에 대여하는 비디오 정보와 대여로를 출력
 			result += "\n\t" + each.getMovie().getTitle() + "\t" +
-					String.valueOf(thisAmount) + "\n";
+					String.valueOf(each.getCharge()) + "\n";
 			// 현재까지 누적된 총 대여료
-			totalAmount += thisAmount;
+			totalAmount += each.getCharge();
 		}
 		// 푸터 행 추가
 		result += "누적 대여료 : " + String.valueOf(totalAmount) + "\n";
@@ -57,29 +48,5 @@ public class Customer {
 		
 		return result;
 	}
-	
-	// 비디오 종류별 대여료 계산 기능을 빼내어 별도의 메소드로 작성
-	private double amountFor(Rental aRental) {
-		double result = 0;
-		switch (aRental.getMovie().getPriceCode()) {
-		case Movie.REGULAR:
-			result += 2;
-			if (aRental.getDaysRented() > 2) {
-				result += (aRental.getDaysRented() - 2) * 1.5;
-			}
-			break;
-			
-		case Movie.NEW_RELEASE: 
-			result += aRental.getDaysRented() * 3;
-			break;
-			
-		case Movie.CHILDRENS:
-			result += 1.5;
-			if (aRental.getDaysRented() > 3) {
-				result += (aRental.getDaysRented() - 3) * 1.5;
-			}
-			break;
-		}
-		return result;
-	}
+
 }
